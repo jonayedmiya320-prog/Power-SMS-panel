402bc82e43e405221ff6c319dc139ceda55e699f
jonayedmiya320-prog <jonayedmiya320@gmail.com>
2026-06-23 01:32:26 +0600

    Update server.js

--- server.js
+++ server.js
@@ -12,5 +12,5 @@ app.get('*', (req, res) => {
 });
 
 app.listen(PORT, '0.0.0.0', () => {
-    console.log(`Server running on port ${PORT}`);
+    console.log(`✅ Server running on port ${PORT}`);
 });
