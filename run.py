a851ab7578af60142d10ea973741fea35b9fa6f6
jonayedmiya320-prog <jonayedmiya320@gmail.com>
2026-06-27 12:39:16 +0600

    Create run.py

---
+++ run.py
@@ -0,0 +1,10 @@
+from app import create_app, db
+
+app = create_app()
+
+if __name__ == '__main__':
+    import os
+    with app.app_context():
+        db.create_all()
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=False)
